a001d168119dbb159c719907c83592ef8fb26493
KennedyOti <otienokennedy972@gmail.com>
2024-05-23 00:33:34 -0600

    my first commit on the python fundamentals

---
+++ Pyathon_Data_Structures.py
@@ -0,0 +1,7 @@
+# DATA STRUCTURES IN PYTHON
+# Data Structures is a way of strong data in programming so that it can be accessed easily.
+# 1Python Lists Explained
+# How to Create a list in Python
+
+students = [12, 23, 34, 45, 46, 56]
+print(students[0])
---
+++ Python_Basics.py
@@ -0,0 +1,5 @@
+fruits = ['mango', 'orange', 'apple', 'banana', 'grapes', 'berry']
+i = 0
+while i < len(fruits):
+     print(fruits[i])
+     i = i+1
